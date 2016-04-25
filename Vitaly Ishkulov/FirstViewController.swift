4750573bfbbb4b0039ea5ad67c7b6c4270e6c3e2
Vitaly Ishkulov <vit@iva-soft.com>
2016-04-25 14:07:01 +0500

    Initial Commit

---
+++ Vitaly Ishkulov/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Vitaly Ishkulov
+//
+//  Created by Vitaly Ishkulov on 4/25/16.
+//  Copyright © 2016 Vitaly Ishkulov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
